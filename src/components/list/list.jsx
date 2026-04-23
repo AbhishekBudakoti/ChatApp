2c39f2aaeac4533e1e03182c62ac39767d4af89a
Abhishek-Budakoti <abhishek.budakoti.04@gmail.com>
2026-04-23 17:29:09 +0530

    fix

--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -1,6 +1,6 @@
 import ChatList from "./chatList/chatList";
 import "./list.css"
-import UserInfo from "./userInfo/userInfo";
+import UserInfo from "./userInfo/userInfo.jsx";
 const List = () => {
     return (
         <div className="list">
